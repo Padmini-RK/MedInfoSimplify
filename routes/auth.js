c3ab90d5f01f52e375770151a30f7b578235bb61
Padmini R K <>
2024-02-09 18:39:30 -0800

    Corrected Indentation

--- routes/auth.js
+++ routes/auth.js
@@ -11,18 +11,30 @@ router.post('/register', async(req, res) => {
         const { userName, email, password } = req.body;
         const userNameExists = await User.findOne({userName});
         if (userNameExists) {
-            return res.status(400).send({message: 'An account with this usename already exists.', success: false});
+            return res.status(400).send({
+                message: 'An account with this usename already exists.',
+                success: false
+            });
         }
         const emailExists = await User.findOne({email});
         if(emailExists) {
-            return res.status(400).send({message: 'An account with this email already exists.', success: false});
+            return res.status(400).send({
+                message: 'An account with this email already exists.',
+                success: false
+            });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({userName, email, password: hashedPassword});
         await user.save();
-        res.status(201).send({ success: true, message: 'User registered successfully!'});
+        res.status(201).send({
+            success: true,
+            message: 'User registered successfully!'
+        });
     } catch (error) {
-        res.status(500).send({success: false, message: error.message})
+        res.status(500).send({
+            success: false,
+            message: error.message
+        })
     }
 });
 
@@ -30,7 +42,11 @@ router.post('/login', async(req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({email});
-        const errorObj = {success: false, error:'Authentication Failed', message: "Check email or password entered"};
+        const errorObj = {
+            success: false,
+            error:'Authentication Failed',
+            message: "Check email or password entered"
+        };
         if (!user){
             return res.status(401). send(errorObj)
         }
@@ -38,8 +54,15 @@ router.post('/login', async(req, res) => {
         if(!passwordMatch){
             return res.status(401).send(errorObj);
         }
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
-        res.status(200).send({ token, success: true});
+        const token = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_SECRET,
+            { expiresIn: '1h'}
+            );
+        res.status(200).send({
+            token,
+            success: true
+        });
     }catch(error){
         res.status(500).send(error.message);
     }
